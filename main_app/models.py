5968641fc8c5ce491cae41ded4e4a246c1483c0c
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-04-08 17:36:33 +0200

    Created project main_app and started creation of data models for Article, Country and Source. Models still need work

---
+++ main_app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from datetime import date
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=250)
+    #articles = models.ManyToManyField(Article, related_name='article_countries')
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Source(models.Model):
+    url = models.CharField(max_length=500)
+    name = models.CharField(max_length=250)
+    origin = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='country_sources')
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name + '-' + self.origin.name
+
+
+class Article(models.Model):
+    url = models.CharField(max_length=500)
+    title = models.TextField()
+    rating = models.IntegerField(default=0)
+    date = models.DateField(default=date.today)
+    length = models.IntegerField(default=0)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name='source_articles')
+    countries = models.ManyToManyField(Country, related_name='country_articles')
+
+    class Meta:
+        ordering = ('-date',)
+
+    def __str__(self):
+        return self.title + '-' +  self.source.name
+
+
+
+
+
+
