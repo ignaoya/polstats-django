75f071a293c7648817f3826f9760b1d86336a559
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-04-19 15:32:49 +0200

    Added settings for djangorestframework and created serializers for country, source and article

---
+++ main_app/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from ..models import Article, Country, Source
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['name']
+
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Source
+        fields = ['url', 'name', 'origin']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['url', 'title', 'rating', 'date', 'length', 'source', 'countries']
