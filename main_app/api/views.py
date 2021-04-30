e68218ea3e392b9dec3a4391d8099562307c439e
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-04-30 14:45:37 +0200

    Modified views in order to remove all permissions from requests. No need to login in order to use the API, this is for developing purposes

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -6,28 +6,34 @@ from .serializers import ArticleSerializer, CountrySerializer, SourceSerializer
 class ArticleListView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = []
 
 
 class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = []
 
 
 class CountryListView(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = []
 
 
 class CountryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = []
 
 
 class SourceListView(generics.ListCreateAPIView):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
+    permission_classes = []
 
 
 class SourceDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
+    permission_classes = []
