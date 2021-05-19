b1d9e05fad2d288d6e28316174cbc6544ba5585c
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-05-19 11:44:18 +0200

    Created basic sentiment analyzer using the nltk SentimentIntensityAnalyzer

---
+++ sentiment/sentiment_analyzer.py
@@ -0,0 +1,10 @@
+import nltk
+from statistics import mean
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+sia = SentimentIntensityAnalyzer()
+
+def get_article_sentiment(article_text: str) -> float:
+    tokenized_sentences = nltk.sent_tokenize(article_text)
+    scores = [sia.polarity_scores(sentences)["compound"] for sentences in tokenized_sentences]
+    return round(mean(scores)*10, 2)
