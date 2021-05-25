af4a1eabf2419e5b608dc59aaad2ed13ad3923c7
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-05-25 11:15:20 +0200

    Added GUI User Login

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ app_name = 'account'
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('login/', views.user_login, name='login'),
     ]
