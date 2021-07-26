c3344175367b95b4decb5381c6cd73aee71aa369
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-07-26 08:39:00 +0200

    tests first commit

---
+++ main_app/api/tests.py
@@ -0,0 +1,9 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Article, Source, Country
+from django.contrib.auth.models import User
+
+
+class ArticleTests(APITestCase):
+    pass
